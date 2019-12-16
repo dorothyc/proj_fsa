659bc5eb879cce842e3f47e700fac0cf92f327aa
Dorothy Cheung <mymelody8448@gmail.com>
2019-12-15 23:45:18 -0800

    Remove print statements and update variables

--- BPI.assignment.js
+++ BPI.assignment.js
@@ -1,10 +1,17 @@
-//let theStates = function () {};
-
 /**
- * Functions for BPI
+ * Functions for BPI Assignment
  * @module model/BPI
  */
 
+
+/**
+ * User-defined error message
+ * @param {string} message
+ */
+function UserException(message) {
+    this.message = message;
+    this.name = 'UserException';
+ }
 
  /**
  * Determine final state using finite state automata
@@ -12,37 +19,39 @@
  * @returns {string} {string}
  */
 exports.findState = function(str) {
+    let currState = 'S0';
+    const validInput = [0, 1];
+    const stateOutput = { "S0": '0', "S1": '1', "S2": '2' };
     try {
         const input = str.split('');
-        const validInput = [0, 1];
-        const stateOutput = { "S0": '0', "S1": '1', "S2": '2' };
-        let currState = 'S0';
-
         input.forEach((chr) => {
             let digit = parseInt(chr);
             if (validInput.includes(digit)) {
                 switch (currState) {
                     case ('S0'):
                         currState = (digit) ? 'S1' : 'S0';
-                        //console.log('S0', digit, currState);
                         break;
                     case ('S1'):
                         currState = (digit) ? 'S0' : 'S2';
-                        //console.log('S1', digit, currState);
                         break;
                     case ('S2'):
                         currState = (digit) ? 'S2' : 'S1';
-                        //console.log('S2', digit, currState);
                         break;
                     default:
                         currState = 'S0';
                 }
             }
+            else {
+                throw new UserException('Input must be 0 or 1');
+            }
         });
-        return stateOutput[currState];
     }
     catch (e){
         console.error('Error with input: ', e.message);
+    }
+    finally {
+        console.log('final state', currState, '=', stateOutput[currState]);
+        return stateOutput[currState];
     }
 };
 
